300f3446fdc84c1c07824cc0510797199ffd53d9
Lucas Rufino <l.rufino@cardpay.local>
2022-07-26 18:02:41 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResources.java
@@ -0,0 +1,31 @@
+package com.devsuperior.hrworker.resources;
+
+import com.devsuperior.hrworker.entities.Worker;
+import com.devsuperior.hrworker.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/workers")
+public class WorkerResources {
+
+    @Autowired
+    private WorkerRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(repository.findById(id).get());
+    }
+
+}
